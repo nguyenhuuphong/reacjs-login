a3864b1772451d576fed1d5a8f710ecc1b2206a7
Glitch (starter-react-template) <none>
2021-01-13 14:05:51 +0000

    🍕🚊 Checkpoint ./src/App.js:25419550/327 ./src/ components/ Header.js:25419550/234

---
+++ src/ components/ Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render(){
+    return (
+      <p className="Header"> đi chợ</p>
+    );
+  }
+  
+}
+
+export default Header 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+
 
 class App extends Component {
   render() {
@@ -8,17 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code>
-          </p>
-          <a
-            className="App-link"
-            href="https://coders-x.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Yêu anh Thịnh
-          </a>
+          
         </header>      
       </div>
     );
